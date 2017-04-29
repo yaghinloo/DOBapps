8f55c7e40efcae58512cfef459295c7cac6dbda3
yaghinloo <yaghinloo@gmail.com>
2017-04-29 16:52:27 -0400

    prject started

--- src/dob.js
+++ src/dob.js
@@ -1,3 +1,37 @@
 /**
- * Created by maryam on 4/29/2017.
+ * Created by Hadi Yaghinloo on 4/29/2017.
  */
+
+class DOB {
+    constructor(container = '#dob_container') {
+        //load JSON from the end point
+        this.JSONRsp = null;
+        this.container = document.querySelector(container);
+        this.headerCollection = [];
+        this.loadDOBJSON();
+    }
+
+// create Table
+    createDOBTable() {
+        this.container.innerHTML = "Project started";
+    }
+// callback on json load
+    setInitialState(data) {
+        this.JSONRsp = data;
+       this.createDOBTable();
+        console.log(data);
+    }
+
+//call the DOB endpoint and load the JSON data
+    loadDOBJSON() {
+        $.getJSON('https://data.cityofnewyork.us/resource/rvhx-8trz.json', (data)=>{
+            this.setInitialState(data)
+        });
+    }
+
+}
+document.addEventListener("DOMContentLoaded",() => {
+    new DOB();
+})
+
+
